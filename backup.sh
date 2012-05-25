a1f5ca8fa26fdcc0b781320ecc636940874cb1fd
Fabian Norman <fabiann@gmail.com>
2012-05-24 17:18:57 -0700

    Created basic framework and flow of the script.

--- backup.sh
+++ backup.sh
@@ -6,6 +6,34 @@
 # Using this script in any manner other than directed is not advisable.
 ########################################################################
 
+# Location of site files
+WWW_DIR="/var/www/"
+
+# MYSQL database info
+MYSQL_USER="backup"
+MYSQL_PASS="password"
+MYSQL_HOST="localhost"
+MYSQL_DATABASE="database"
+
+# Location to store the backups
+BACKUP_DIR="~/backup/"
+
+
+# First, lets setup a temp location where the files will go before being tar'd.
+
+TODO: check if temp folder already there, if so remove it. Then create the temp folder.
+
+
+# Now lets copy the web files over to the temp location
+
+
+# And now lets create a MYSQL dump of the wanted database. Then copy it to the temp location.
+
+
+# Now lets take all the contents of the temp location and tar it into the desired backup location.
+
+
+
 
 
 
