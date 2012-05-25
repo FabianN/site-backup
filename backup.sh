84e7278954a4ca9db38da44637b8a8aff172625d
Fabian Norman <fabiann@gmail.com>
2012-05-24 17:10:46 -0700

    Initial Commit.

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Website backup
+########################################################################
+# Website backup script made by Fabian Norman (fnorman@uoregon.edu, FabianN@gmail.com)
+# This script is designed to backup the files and the MySQL database for a web site. The backup will be placed into a dated tarball.
+# Using this script in any manner other than directed is not advisable.
+########################################################################
+
+
+
+
