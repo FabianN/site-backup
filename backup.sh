821341962552542a6a98da06c7111c9e2b1690fc
Fabian Norman <fabiann@gmail.com>
2012-05-27 15:26:38 -0700

    Adjusting comment wording.

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 ########################################################################
 # Website backup script made by Fabian Norman (fnorman@uoregon.edu,
 # FabianN@gmail.com). This script is designed to backup the files and
-# the MySQL database for a web site. The backup will be placed into a
-# dated tarball. Using this script in any manner other than directed
+# the MySQL database of a web site. The backup will be placed into a
+# dated tar. Using this script in any manner other than directed
 # is not advisable.
 ########################################################################
 
